9db2493a4026a1fd5ca995664d7acf9b9485dca1
Amine <abalimohammedamine@gmail.com>
2024-04-11 15:59:39 +0200

    Add JDBC.

---
+++ src/main/java/fr/hetic/DatabaseToFile.java
@@ -0,0 +1,49 @@
+package fr.hetic;
+import java.sql.*;
+
+public class DatabaseToFile {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://SG-hetic-mt4-java-5275-pgsql-master.servers.mongodirector.com:5432/TP";
+        String user = "etudiant";
+        String password = "MT4@hetic2324";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            String sql = "SELECT F.NOM, L.PARAM1, L.PARAM2, L.OPERATEUR, L.INDEX " +
+                    "FROM FICHIER F " +
+                    "INNER JOIN LIGNE L ON F.ID = L.FICHIER_ID " +
+                    "WHERE F.TYPE = 'OP'";
+
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(sql)) {
+
+                while (resultSet.next()) {
+                    String nomFichier = resultSet.getString("NOM");
+                    int param1 = resultSet.getInt("PARAM1");
+                    int param2 = resultSet.getInt("PARAM2");
+                    String operateur = resultSet.getString("OPERATEUR");
+                    int index = resultSet.getInt("INDEX");
+
+                    // Générer le nom du fichier par convention
+                    String nomFichierFinal = generateFileName(nomFichier, param1, param2);
+
+                    // Écrire les données dans le fichier
+                    writeToFile(nomFichierFinal, param1, param2, operateur, index);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String generateFileName(String nomFichier, int param1, int param2) {
+        // Implémentez la logique pour générer le nom du fichier par convention
+        // Par exemple, vous pouvez concaténer le nom du fichier avec les paramètres
+        return nomFichier + "_" + param1 + "_" + param2 + ".txt";
+    }
+
+    private static void writeToFile(String fileName, int param1, int param2, String operateur, int index) {
+        // Implémentez la logique pour écrire les données dans le fichier
+        // Assurez-vous de gérer les erreurs possibles lors de l'écriture dans le fichier
+        // Vous pouvez utiliser BufferedWriter ou d'autres classes Java pour l'écriture de fichiers
+    }
+}
